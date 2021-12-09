0aaeeacde4935bc55dc6584476ceaab32486f2a0
dameko sayama <nrtk.emerald503@gmail.com>
2021-12-10 06:53:37 +0900

    tdb
    cant load that xml to study

--- sample17.php
+++ sample17.php
@@ -1,6 +1,8 @@
 <?php 
-
-$xmltree = simplexml_load_file('rss.xml');
-echo $xmltree->channel->title;
-
+$xmlTree = simplexml_load_file('data/rss.xml');
+foreach ($xmlTree->channel->item as $item):
+?>
+.<a href = "<?php echo $item->link ?>"<?php echo $item->title; ?><br>
+<?php
+endforeach;
 ?>
